e1fc2dd291f6ceba3d50f193f4150451486b9687
Miles Whiticker <miles.whiticker@gmail.com>
2024-10-15 14:22:32 +1000

    fix a bug with retreat and withdraw modes using a pseudo move action which expects a paywindow to be open when it resolves (and tries to close the paywindow)
    Signed-off-by: Miles Whiticker <miles.whiticker@gmail.com>

--- modules/ui_payment.js
+++ modules/ui_payment.js
@@ -480,11 +480,15 @@ define(
 			
 			DestroyPayWindow : function()
 			{
-				dojo.removeClass("paywindow","paywindow_slidein");
-				dojo.removeClass("paywindow","paywindow_minimise");
-				dojo.removeClass("paywindow","paywindow_unminimise");
-				dojo.addClass("paywindow","paywindow_slideout");
-				//dojo.destroy(dojo.byId("paywindow"));
+				var paywindow = dojo.byId("paywindow");
+				if(paywindow)
+				{
+					dojo.removeClass("paywindow","paywindow_slidein");
+					dojo.removeClass("paywindow","paywindow_minimise");
+					dojo.removeClass("paywindow","paywindow_unminimise");
+					dojo.addClass("paywindow","paywindow_slideout");
+					//dojo.destroy(dojo.byId("paywindow"));
+				}
 				this.payment_mode = gameui.STATE_INVALID;
 			},
 			
